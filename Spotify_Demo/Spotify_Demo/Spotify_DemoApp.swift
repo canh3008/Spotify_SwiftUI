9d1ba2882b9a62809cfc64f2cad9a8551d97f109
Duc Canh <tranduccanh3008@gmail.com>
2024-08-09 09:53:42 +0700

    create app

---
+++ Spotify_Demo/Spotify_Demo/Spotify_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_DemoApp.swift
+//  Spotify_Demo
+//
+//  Created by Duc Canh on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
